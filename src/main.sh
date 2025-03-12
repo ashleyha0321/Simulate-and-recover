3d7f6291bce1bf3454babb2bb566ebc6ce0888cc
ashleyha <haak2@uci.edu>
2025-03-12 06:32:32 +0000

    removed unnecessary comments

--- src/main.sh
+++ src/main.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 set -e
-
-# Navigate to the script directory
 cd "$(dirname "$0")"
-
-# Run the EZ Diffusion Model simulation
-echo "Running EZ Diffusion Model simulation..."
 /usr/bin/python3 ../src/EzDiffusionModel.py
 
 
